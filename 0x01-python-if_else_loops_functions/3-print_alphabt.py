7940d33aede3b7f995bafea2ee69b8c39e9cfab8
Lotizzle <lotharmbaegbu@gmail.com>
2023-09-05 20:47:28 -0700

    Printing the alphabet in lowercase without select letters

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""prints the alphabet in lowercase except the letters q and e"""
+for letter in range(97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)),end="")
