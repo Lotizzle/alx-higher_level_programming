b463afc1d03531e3127b85aa6b24f9faa85b5ca4
Lotizzle <lotharmbaegbu@gmail.com>
2023-09-06 01:52:53 -0700

    Fixing minor syntax error

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 """prints the alphabet in lowercase without adding a new line."""
 for letter in range(97, 123):
-    print("{}".format(chr(letter)),end="")
+    print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,4 @@
 """prints the alphabet in lowercase except the letters q and e"""
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
-        print("{}".format(chr(letter)),end="")
+        print("{}".format(chr(letter)), end="")
