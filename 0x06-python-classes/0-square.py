17e241c0f3e5e39cea000dd160c0e0bc6e343d1e
Lotizzle <lotharmbaegbu@gmail.com>
2023-09-28 15:25:33 -0700

    Creating class Square

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" This is a file to creat a Class called Square and
+its possible methods, objects or attributes
+"""
+
+class Square:
+
+    """ Square class:
+    
+    This is an empty class Square that defines a square
+    """
+
+    pass
---
+++ 0x06-python-classes/README.md
@@ -0,0 +1,2 @@
+## 0x06. Python - Classes and Objects
+### Learning about classes and attributes
