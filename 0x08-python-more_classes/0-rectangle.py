daf80ea21d87eda914285e1c5c2c61990b1d5139
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-02 20:16:07 -0700

    Created a Rectangle class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+""" This creates an empty class called a Rectangle """
+
+class Rectangle:
+
+    """ This class only contains the pass statement for now. """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+## 0x08. Python - More Classes and Objects
+### Learning more about OOP, classes and objects in python programming.
