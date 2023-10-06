abd7f2fa7f2d409de87f94756f99a3f67259ae02
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-06 05:44:32 -0700

    Running first doctest

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines a integer addition function."""
+
+def add_integer(a, b=98):
+    """ This function perfoms the addition of a and b.
+    a and b are first casted to integers if they are floats.
+
+    Raises:
+        TypeError: if either a or b isn't a type int or float.
+    """
+    
+    if((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a)+int(b))
