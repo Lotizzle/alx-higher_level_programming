3bd4b7604b01dec681f90230cbd178cef2b2debd
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-09 17:49:20 -0700

    Returning a list of the available attr and methods

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module contains only one function (lookup)."""
+
+
+def lookup(obj):
+    """The lookup function returns a list of the attributes
+    and methods of an object."""
+
+
+    return (dir(obj))
