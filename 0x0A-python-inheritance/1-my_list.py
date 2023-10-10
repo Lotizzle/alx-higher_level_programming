af91e5d11d80364132afc8353efaa924be22baf7
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-09 19:43:28 -0700

    Printing sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,10 +7,8 @@ a method called ``def print_sorted(self)``."""
 class MyList(list):
     """ This class is a subclass of the built-in list class."""
 
-
     def print_sorted(self):
 
         """This method returns a sorted list in ascending order."""
 
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
