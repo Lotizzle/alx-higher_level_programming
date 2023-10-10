a5de17232b7fa5ba2c3f07fbe2de87e463e3308c
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-09 21:13:54 -0700

    Checking Inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,10 +5,10 @@
 
 def inherits_from(obj, a_class):
 
-    """Checks if obj is an instance of a class inherited from 
+    """Checks if obj is an instance of a class inherited from
     a_class."""
 
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
-        False
+        return False
