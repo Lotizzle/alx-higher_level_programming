332811aa66789d3f99dd5e769205700626a88e2a
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-16 13:11:48 -0700

    Validation check for Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -5,7 +5,7 @@ from models.base import Base
 
 class Rectangle(Base):
     """This class represents a rectangle."""
-    
+
     def __init__(self, width, height, x=0, y=0, id=None):
         """This method initializes a rectangle.
 
@@ -17,11 +17,28 @@ class Rectangle(Base):
             id(int): The identity of the rectangle.
         """
 
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        super().__init__(id)
+        if type(width) != int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        elif type(height) != int:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        elif type(x) != int:
+            raise TypeError("x must be an integer")
+        elif x < 0:
+            raise ValueError("x must be >= 0")
+        elif type(y) != int:
+            raise TypeError("y must be an integer")
+        elif y < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.width = width
+            self.height = height
+            self.x = x
+            self.y = y
+            super().__init__(id)
 
     @property
     def width(self):
