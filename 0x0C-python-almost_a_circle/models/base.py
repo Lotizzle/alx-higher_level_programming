a933df8c9bf3d3e141b5ebe93ffd39e8a9ed19ab
Lotizzle <lotharmbaegbu@gmail.com>
2023-10-17 16:05:19 -0700

    Added create method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -49,3 +49,15 @@ class Base:
         if json_string is None or json_string == "[]":
             return []
         return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes already set."""
+
+        if dictionary or dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                instance = cls(1, 2)
+            else:
+                instance = cls(1)
+            instance.update(**dictionary)
+            return instance
