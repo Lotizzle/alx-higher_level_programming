3b2101a02188da7789e06fb856bc69a692629515
Lotizzle <lotharmbaegbu@gmail.com>
2023-12-20 13:24:28 -0800

    Created relationship between State and City class

---
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module contains the class definition of a City
+"""
+from relationship_state import Base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class City(Base):
+    """
+    This class defines each city
+
+    Attribute:
+        id: City id number
+        name: City name
+        state_id: State id(foreign)
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
