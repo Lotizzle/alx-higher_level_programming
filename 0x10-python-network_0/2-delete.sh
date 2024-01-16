5627a999f517f0bab40ae1d3b3352cf4b41ff2df
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-01-16 02:42:48 -0800

    Fixed minor issues

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a DELETE request to the URL passed as the first argument and displays the body of the response
-response=$(curl -s -X DELETE "$1"); echo "$response"
+curl -sX DELETE "$1"
