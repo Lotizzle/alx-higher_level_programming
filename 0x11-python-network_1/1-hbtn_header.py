673bc26ce598f190e5aa087491d5e69827798334
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-01-16 15:53:08 -0800

    Fetching specific header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,8 +6,11 @@
 import urllib.request
 import sys
 
-
-with urllib.request.urlopen(sys.argv[1]) as response:
-    if 'X-Request-Id' in response.headers:
-        request_id = response.headers['X-Request-Id']
-        print(request_id)
+try:
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        if 'X-Request-Id' in response.headers:
+            request_id = response.headers['X-Request-Id']
+            print(request_id)
+except urllib.error.URLError as e:
+    print("Error:", e.reason)
+    sys.exit(1)
