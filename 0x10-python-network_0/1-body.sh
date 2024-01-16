6539354aa3801a718772d66b9c218aa982848404
“Lotizzle” <lotharmbaegbu@gmail.com>
2024-01-16 03:56:35 -0800

    POSTING mail

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a GET request to the URL, and displays the body of the response
-response=$(curl -sI "$1"); status_code=$(echo "$response" | grep -i "HTTP" | awk '{print $2}'); body=$(echo "$response" | sed '$d'); [ "$status_code" -eq 200 ] && echo "$body"
+curl -sw "%{http_code}" "$1" | 
